e8748c4358919437edfe295cee515f65983f0c73
lannt-xyz <lannt.xyz@gmail.com>
2026-01-21 16:11:23 +0700

    change db structure

--- alembic/env.py
+++ alembic/env.py
@@ -1,18 +1,21 @@
+import inspect
 from logging.config import fileConfig
-
+from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from sqlalchemy import create_engine
 
 from alembic import context
-import sys
-import os
 
-# Add the parent directory to sys.path to import app module
-sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+from app.database import Base
+
+from app.config import config as settings
+
+# Import all models from app/models directory in many of python files
+import app.models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -21,8 +24,6 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from app.database import Base
-from app.config import config as app_config
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
@@ -43,7 +44,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = app_config.DATABASE_URL
+    url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -62,7 +63,11 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = create_engine(app_config.DATABASE_URL, poolclass=pool.NullPool)
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
 
     with connectable.connect() as connection:
         context.configure(
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,21 @@
-# Models package
+import pkgutil
+import importlib
+import inspect
+from app.database import Base # Hoặc nơi bạn định nghĩa Base
+
+# Lấy đường dẫn của thư mục hiện tại (app/models)
+__all__ = []
+
+for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
+    # Import module động
+    full_module_name = f"{__name__}.{module_name}"
+    module = importlib.import_module(full_module_name)
+    
+    # Duyệt qua các thành viên trong module
+    for name, obj in inspect.getmembers(module):
+        # Kiểm tra nếu là class và kế thừa từ Base
+        if inspect.isclass(obj) and issubclass(obj, Base) and obj is not Base:
+            # Export class này ra namespace của package models
+            globals()[name] = obj
+            if name not in __all__:
+                __all__.append(name)
